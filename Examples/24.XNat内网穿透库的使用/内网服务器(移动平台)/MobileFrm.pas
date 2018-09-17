6e092a9506a919ab289b650fcffe798b1d7a58f5
PassByYou888 <197090@qq.com>
2018-09-17 12:53:33 +0800

    update XNat on android-armv7

--- Examples/24.XNat内网穿透库的使用/内网服务器(移动平台)/MobileFrm.pas
+++ Examples/24.XNat内网穿透库的使用/内网服务器(移动平台)/MobileFrm.pas
@@ -40,17 +40,13 @@ end;
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   AddDoStatusHook(Self, DoStatusIntf);
-  PostProgressEng := TNProgressPostWithCadencer.Create;
 
-  PostProgressEng.PostExecute(1.0, procedure(LSender: TNPostExecute)
-    begin
-    end);
-      XCli := TXNATClient.Create;
-      XCli.RemoteTunnelAddr := '127.0.0.1';          // 公网服务器的IP
-      XCli.RemoteTunnelPort := '7890';                  // 公网服务器的端口号
-      XCli.AuthToken := '123456';                       // 协议验证字符串
-      XCli.AddMapping('127.0.0.1', '80', 'web8000'); // 将公网服务器的8000端口反向代理到本地80端口
-      XCli.OpenTunnel;                                  // 启动内网穿透
+  XCli := TXNATClient.Create;
+  XCli.RemoteTunnelAddr := '127.0.0.1';          // 公网服务器的IP
+  XCli.RemoteTunnelPort := '7890';               // 公网服务器的端口号
+  XCli.AuthToken := '123456';                    // 协议验证字符串
+  XCli.AddMapping('127.0.0.1', '80', 'web8000'); // 将公网服务器的8000端口反向代理到本地80端口
+  XCli.OpenTunnel;                               // 启动内网穿透
 end;
 
 procedure TForm1.DoStatusIntf(AText: SystemString; const ID: Integer);
