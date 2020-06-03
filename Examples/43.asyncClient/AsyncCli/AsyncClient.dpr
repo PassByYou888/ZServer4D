27305e3b9177b4b37b5bd5f0d1c073a28571ee7e
PassByYou888 <197090@qq.com>
2020-06-04 07:06:43 +0800

    Update AsyncClient.dpr

--- Examples/43.asyncClient/AsyncCli/AsyncClient.dpr
+++ Examples/43.asyncClient/AsyncCli/AsyncClient.dpr
@@ -10,6 +10,6 @@ begin
   System.ReportMemoryLeaksOnShutdown := True;
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  AApplication.CreateForm(TAsyncClientForm, AsyncClientForm);
-  pplication.Run;
+  Application.CreateForm(TAsyncClientForm, AsyncClientForm);
+  Application.Run;
 end.
