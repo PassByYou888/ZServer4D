1423d7bf3d348f8c7d0dbb389b902e2b3fac348b
PassByYou888 <passbyyou888@outlook.com>
2021-07-22 02:36:01 +0800

    Update BaiduTranslateAPI.pas

--- ServiceApiModel/百度翻译服务/BaiduTranslateAPI.pas
+++ ServiceApiModel/百度翻译服务/BaiduTranslateAPI.pas
@@ -1,15 +1,27 @@
 { * 外部api支持，百度翻译服务http支持                                          * }
-{ * https://github.com/PassByYou888/CoreCipher                                 * }
+{ ****************************************************************************** }
+{ * https://zpascal.net                                                        * }
+{ * https://github.com/PassByYou888/zAI                                        * }
 { * https://github.com/PassByYou888/ZServer4D                                  * }
-{ * https://github.com/PassByYou888/zExpression                                * }
-{ * https://github.com/PassByYou888/zTranslate                                 * }
+{ * https://github.com/PassByYou888/PascalString                               * }
+{ * https://github.com/PassByYou888/zRasterization                             * }
+{ * https://github.com/PassByYou888/CoreCipher                                 * }
 { * https://github.com/PassByYou888/zSound                                     * }
+{ * https://github.com/PassByYou888/zChinese                                   * }
+{ * https://github.com/PassByYou888/zExpression                                * }
+{ * https://github.com/PassByYou888/zGameWare                                  * }
+{ * https://github.com/PassByYou888/zAnalysis                                  * }
+{ * https://github.com/PassByYou888/FFMPEG-Header                              * }
+{ * https://github.com/PassByYou888/zTranslate                                 * }
+{ * https://github.com/PassByYou888/InfiniteIoT                                * }
+{ * https://github.com/PassByYou888/FastMD5                                    * }
 { ****************************************************************************** }
 unit BaiduTranslateAPI;
 
 interface
 
-uses Classes, CoreClasses, PascalStrings, UnicodeMixedLib, MemoryStream64, ZS_JsonDataObjects;
+uses Classes, CoreClasses, PascalStrings, UnicodeMixedLib, MemoryStream64,
+  ZS_JsonDataObjects;
 
 var
   // 使用下列地址申请百度翻译
